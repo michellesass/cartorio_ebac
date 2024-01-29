588fab03705e949a1e48a37acb8f29a02c9b9414
Michelle Sass <149545087+MiniMizinh4@users.noreply.github.com>
2024-01-29 16:36:34 -0300

    Validação por senha
    Foi adicionado a validação por senha no back-end para login.

--- Cartorio.c
+++ Cartorio.c
@@ -209,49 +209,68 @@ int main(){
 	
 	int opcao = 0; //Definindo as variáveis
 	int laco = 1;
+	char senhaDigitada[10] = "a";
+	int comparacao;
 	
-	for(laco = 1; laco == 1;){ //Início do laço de repetição (sempre volta ao menu)
+	setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
+	
+	printf("### Cartório da EBAC ### \n\n");
+	printf("Login de administrador! \n\nDigite a sua senha: ");
+	scanf("%s", senhaDigitada);
+	
+	comparacao = strcmp(senhaDigitada, "admin");
+	
+	if(comparacao == 0){
 		
 		system("cls");
 		
-		setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem
-	
-		printf("### Cartório da EBAC ### \n\n"); //Início do menu
-		printf("Escolha a opção desejada do menu: \n\n");
-		printf("\t1- Registrar nomes \n");
-		printf("\t2- Consultar nomes \n");
-		printf("\t3- Deletar nomes \n"); 
-		printf("\t4- Sair do sistema \n\n");
-		printf("Opção: ");//Fim do menu
-	
-		scanf("%d", &opcao); //Armazenando a escolha do usuário
-	
-		system("cls"); //Responsável por limpar a tela
+		for(laco = 1; laco == 1;){ //Início do laço de repetição (sempre volta ao menu)
 		
-		switch(opcao){ //Início da escolha de opções
-			case 1: 
-				registro(); //chamada de funções
-				break;
-			
-			case 2:
-				consulta();
-				break;
-			
-			case 3:
-				deletar();
-				break;
-			
-			case 4:
-				printf("Obrigado por utilizar o sistema! \n");
-				return 0;
-				break;
-							
-			default:
-				printf("Essa opção não está disponível.\n\n");
-				system("pause");
-				break;						
-		} //Fim do switch (esolha das opções)
+			system("cls");
+		
+			printf("### Cartório da EBAC ### \n\n"); //Início do menu
+			printf("Escolha a opção desejada do menu: \n\n");
+			printf("\t1- Registrar nomes \n");
+			printf("\t2- Consultar nomes \n");
+			printf("\t3- Deletar nomes \n"); 
+			printf("\t4- Sair do sistema \n\n");
+			printf("Opção: ");//Fim do menu
 	
-	} //Fim do laço de repetição
+			scanf("%d", &opcao); //Armazenando a escolha do usuário
+	
+			system("cls"); //Responsável por limpar a tela
+		
+			switch(opcao){ //Início da escolha de opções
+				case 1: 
+					registro(); //chamada de funções
+					break;
+			
+				case 2:
+					consulta();
+					break;
+			
+				case 3:
+					deletar();
+					break;
+			
+				case 4:
+					printf("Obrigado por utilizar o sistema! \n");
+					return 0;
+					break;
+							
+				default:
+					printf("Essa opção não está disponível.\n\n");
+					system("pause");
+					break;						
+			} //Fim do switch (esolha das opções)
+	
+		} //Fim do laço de repetição
+	} //Fim do if
+	
+	else{
+		printf("\nSenha incorreta.\n\n");
+	}
+			
+	
 	
 } //Fim do programa
